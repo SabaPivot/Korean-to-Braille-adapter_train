091e5c76621ba595abd52732f6ebe6cf5dd73dc7
SabaPivot <careforme.dropout@gmail.com>
2024-11-29 01:10:00 +0900

    Refactor: added validation_data_path argument parser and reused json reading function from utils.py

--- train/validation_adapter_train.py
+++ train/validation_adapter_train.py
@@ -5,12 +5,13 @@ from tqdm import tqdm
 from peft import PeftModel, PeftConfig
 import json
 import argparse
-from ..utils import translate_text, eval
+from ..utils import translate_text, eval, read_korean_braille_pairs
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--model_name", type=str, default="VEXFZU/t5-xlarge-ko-kb", required=True, help="Path to the model checkpoint.")
 parser.add_argument("--revision", type=str, default="main", required=False, help="Revision name for the checkpoint.")
 parser.add_argument("--peft_model", type=str)
+parser.add_argument("--validation_data_path", type=str, default="validation_data.json", required=False, help="Path to the validation data.")
 
 args = parser.parse_args()
 model_name = args.model_name
@@ -21,14 +22,7 @@ model = AutoModelForSeq2SeqLM.from_pretrained(model_name, revision=revision)
 tokenizer = AutoTokenizer.from_pretrained(model_name, revision=revision)
 model = PeftModel.from_pretrained(model, peft_model_id)
 
-inputs, targets = (
-    ["<도시와 그 불확실한 벽>이 기나긴 추석 연휴 기간 교보문고 베스트셀러 일 위에 올랐다.",
-    "한국의 웹 드라마 《고래 먼지》다.",
-    "『신경숙의 엄마를 부탁해』 중에서"], 
-    ["⠐⠶⠊⠥⠠⠕⠧⠀⠈⠪⠀⠘⠯⠚⠧⠁⠠⠕⠂⠚⠒⠀⠘⠱⠁⠶⠂⠕⠀⠈⠕⠉⠈⠟⠀⠰⠍⠠⠹⠀⠡⠚⠩⠀⠈⠕⠫⠒⠀⠈⠬⠘⠥⠑⠛⠈⠥⠀⠘⠝⠠⠪⠓⠪⠠⠝⠂⠐⠎⠀⠕⠂⠀⠍⠗⠝⠀⠥⠂⠐⠣⠌⠊⠲",
-    "⠚⠒⠈⠍⠁⠺⠀⠏⠗⠃⠀⠊⠪⠐⠣⠑⠀⠰⠶⠈⠥⠐⠗⠀⠑⠾⠨⠕⠶⠆⠊⠲",
-    "⠰⠦⠠⠟⠈⠻⠠⠍⠁⠺⠀⠎⠢⠑⠐⠮⠀⠘⠍⠓⠁⠚⠗⠴⠆⠀⠨⠍⠶⠝⠠⠎"]
-)
+inputs, targets = read_korean_braille_pairs()
 targets = [elem.replace('⠀', ' ').replace('\u2800', ' ') for elem in targets]
 outputs = []
 start = time()
